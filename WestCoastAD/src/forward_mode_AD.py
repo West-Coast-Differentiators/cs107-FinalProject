db5da60c625eb4064c9c9e2287ccd37df2e7b9fe
anita76 <anita.mahinpei@gmail.com>
2020-11-16 03:00:56 -0800

    documentation and setup files

--- WestCoastAD/src/forward_mode_AD.py
+++ WestCoastAD/src/forward_mode_AD.py
@@ -1,10 +1,40 @@
 import numpy as np 
 
 class Variable:
+    """
+    This is a custom variable class with elementary function and operation overloading
+    to perform automatic differentiation.
+
+    EXAMPLES
+    =========
+
+    # Derivative computation for a single input scalar function
+    >>> x = Variable(4, 1)
+    >>> f = 3*x**2 + 3
+    >>> f.value
+    51.0
+    >>> f.derivative
+    24.0
+
+    """
 
     def __init__(self, value, derivative_seed):
+        """ 
+        constructor for the Variable class
+
+        INPUTS
+        =======
+        value: An int or float giving the value of the variable
+        derivative_seed: An int or float giving a seed value for the variable derivative
+
+        """
+
         self.value = value
         self.derivative = derivative_seed
+    
+    
+    def __repr__(self):
+        return "Variable(value: {}, derivative: {})".format(self.value, self.derivative)
         
     @property
     def value(self):
@@ -28,9 +58,27 @@ class Variable:
         else:
             self._derivative = derivative_seed
         
-        
 
     def __add__(self, other):
+        """ 
+        Dunder method for overloading the "+" operator. 
+        Computes the value and the derivative of the summation operation
+
+        INPUTS
+        =======
+        other: a Variable object, an int, or a float
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the summation operation.
+
+        NOTES
+        =====
+        POST:
+         - self is not changed by this function
+
+        """
+
         try:
             val = self.value + other.value
             der = self.derivative + other.derivative
@@ -40,7 +88,26 @@ class Variable:
         
         return Variable(val, der)
 
+
     def __radd__(self, other):
+        """ 
+        Dunder method for overloading the "+" operator. 
+        Computes the value and derivative of the summation operation
+
+        INPUTS
+        =======
+        other: a Variable object, an int, or a float
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the summation operation.
+
+        NOTES
+        =====
+        POST:
+         - self is not changed by this function
+
+        """
         return self.__add__(other)
         
     
@@ -49,15 +116,6 @@ class Variable:
     
     def __rmul__(self, other):
         pass
-    
-    # def __sub__(self, other):
-        # try:
-        #     val = self.value - other.value
-        #     der = self.derivative - other.derivative
-        # except AttributeError:
-        #     val = self.value - other
-        #     der = self.derivative
-    
         
     def __sub__(self, other):
         return self + (-other)
@@ -96,6 +154,24 @@ class Variable:
         pass
     
     def sin(self):
+        """ 
+        Computes the value and derivative of the sin function
+
+        INPUTS
+        =======
+        None
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the sin function.
+
+        NOTES
+        =====
+        POST:
+         - self is not changed by this function
+
+        """
+
         val = np.sin(self.value)
         der = np.cos(self.value) * self.derivative
         return Variable(val, der)
@@ -107,6 +183,28 @@ class Variable:
     
     
     def tan(self):
+        """ 
+        Computes the value and derivative of the tan function
+
+        INPUTS
+        =======
+        None
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the tan function.
+
+        NOTES
+        =====
+        PRE:
+         -  self.value is not an odd multiple of pi/2 otherwise a ValueError will be raised
+        POST:
+         - self is not changed by this function
+
+        """
+        if (self.value / (np.pi/2)) % 2 == 1:
+            raise ValueError("Inputs to tan should not be odd multiples of pi/2")
+
         val = np.tan(self.value)
         der = self.derivative / np.cos(self.value)**2
         return Variable(val, der)
@@ -114,6 +212,23 @@ class Variable:
     ## Other functions beyond the basics
 
     def sinh(self):
+        """ 
+        Computes the value and derivative of the sinh function
+
+        INPUTS
+        =======
+        None
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the sinh function.
+
+        NOTES
+        =====
+        POST:
+         - self is not changed by this function
+
+        """
         val = np.sinh(self.value)
         der = np.cosh(self.value) * self.derivative
         return Variable(val, der)
@@ -127,6 +242,29 @@ class Variable:
         pass
     
     def arcsin(self):
+        """ 
+        Computes the value and derivative of the arcsin function
+
+        INPUTS
+        =======
+        None
+
+        RETURNS
+        ========
+        a Variable object with the derivative and value of the arcsin function.
+
+        NOTES
+        =====
+        PRE:
+         - self.value should be in [-1, 1], otherwise a ValueError will be raised
+        POST:
+         - self is not changed by this function
+
+        """
+
+        if self.value >1 or self.value < -1:
+            raise ValueError("Inputs to arcsin should be in [-1, 1].")
+        
         val = np.arcsin(self.value)
         der = self.derivative / np.sqrt(1-self.value**2)
         return Variable(val, der)
