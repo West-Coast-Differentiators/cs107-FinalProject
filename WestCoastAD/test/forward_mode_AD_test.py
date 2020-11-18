986a5b9c4a0321a059874d2a8482cdc2e6ad2017
Kire Semada <egadames990@gmail.com>
2020-11-18 14:42:15 -0800

    fixed intergration test bugs and added truediv tanh test

--- WestCoastAD/test/forward_mode_AD_test.py
+++ WestCoastAD/test/forward_mode_AD_test.py
@@ -392,7 +392,22 @@ class VariableIntegrationTest(unittest.TestCase):
         
         self.assertEqual(np.arcsin(value1) - np.sin(value2), equation.value)
         self.assertEqual(expected_derivative, equation.derivative)
+    
+    def test_truediv_and_tanh_scalar(self):
+        value = np.pi / 4
+        var = Variable(value, 1)
+        equation = var / np.tanh(var)
+        equation2 = np.tanh(var) / var
 
+        self.assertEqual(value / np.tanh(value), equation.value)
+        expected_derivative = (np.tanh(value) * 1 - value * (1 / (np.cosh(value) **2))) / (np.tanh(value) **2)
+        self.assertEqual(expected_derivative, equation.derivative)
+
+        # Test for rtruediv
+        self.assertEqual(np.sin(value) / value, equation2.value)
+        expected_derivative2 = (value * (1 / (np.cosh(value) **2) * 1) - np.tanh(value)*1) / (value **2)
+        self.assertEqual(expected_derivative2, equation2.derivative)
+    
     def test_mul_and_tanh_scalar(self):
         value = np.pi / 3
         var = Variable(value, 1)
@@ -435,7 +450,7 @@ class VariableIntegrationTest(unittest.TestCase):
         equation2 = np.tan(var) / var
 
         self.assertEqual(value / np.tan(value), equation.value)
-        expected_derivative = np.tan(value) * 1 - value * (1 / (np.cos(value) **2)) / (np.tan(value) **2)
+        expected_derivative = (np.tan(value) * 1 - value * (1 / (np.cos(value) **2))) / (np.tan(value) **2)
         self.assertEqual(expected_derivative, equation.derivative)
 
         # Test for rtruediv
