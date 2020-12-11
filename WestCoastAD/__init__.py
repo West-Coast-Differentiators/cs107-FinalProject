44cb1e6c0717f52461e255758841d130d515035e
Yingchen Liu <yingchenliu@Yingchens-MacBook-Pro.local>
2020-12-10 23:38:14 -0700

    Modified init.

--- WestCoastAD/__init__.py
+++ WestCoastAD/__init__.py
@@ -2,4 +2,8 @@ from .src.forward_mode_AD import Variable
 from .src.forward_mode_AD_auxiliaries import vector_function_jacobian, vector_function_value, multivariate_dimension_check
 from .src.optimizer import Optimizer
 from .src.initializer import Initializer
-
+from .src.initializer import Zeros
+from .src.initializer import Ones
+from .src.initializer import Constant
+from .src.initializer import RandomUniform
+from .src.initializer import RandomNormal
