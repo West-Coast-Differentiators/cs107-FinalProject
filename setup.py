3e0a31788e4eaf6be17e0ba314db851fdde6da4f
Kire Semada <egadames990@gmail.com>
2020-12-11 14:54:10 -0800

    updated readme and setup.py for pip setup

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
+import setuptools
 
-setup(name='WestCoastAD',
-      version='1.0',
-      packages=setuptools.find_packages(),
-      install_requires = [ 'numpy>=1.18.5' ]
-      )
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='WestCoastAD-era998_test44',
+      version='1.3',
+      packages= setuptools.find_packages(),
+      author="Anita Mahinpei, Yingchen Liu, Erik Adames, Lekshmi Santhosh",
+      description="An Optimization Package with an Automatic Differenation Core",
+      long_description= long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/West-Coast-Differentiators/cs107-FinalProject",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
+      install_requires = [ 'numpy==1.19.3' ]
+)
