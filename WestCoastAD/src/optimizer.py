61dca51fd619527a3a0c21d8fa1a22d85b839fb3
anita76 <anita.mahinpei@gmail.com>
2020-12-11 13:36:19 -0800

    add spacing that was removed

--- WestCoastAD/src/optimizer.py
+++ WestCoastAD/src/optimizer.py
@@ -15,6 +15,7 @@ class Optimizer():
     def __init__(self, objective_function, variable_initialization, scalar=True):
         """
         constructor for the Optimizer class.
+
         INPUTS
         =======
         - objective_function: a python function that takes as input a single vector or one or more scalars and
@@ -23,9 +24,11 @@ class Optimizer():
         - variable_initialization: a 1D numpy array of floats/ints containing initial values for the inputs to the 
                 objective function
         - scalar: True if the inputs to objective_function are one or more scalars otherwise False; Default is True
+
         RETURNS
         ========
         None
+
         NOTES
         =====
         Pre:
@@ -33,6 +36,7 @@ class Optimizer():
          - variable_values has the same length as the number of inputs to objective_fuctnion if objective_function takes 
                 scalar inputs, or the length of the vector input to objective_function.
          - variable_values must be in the same order as the inputs to func
+
         EXAMPLES
         =========
         
@@ -40,7 +44,7 @@ class Optimizer():
         >>> import numpy as np
         >>> f = lambda x, y: x**2 + y**2
         >>> op = Optimizer(f, np.array([1, -1]))
-        
+
         # multivariate function with a vector as input
         >>> import numpy as np
         >>> f = lambda x: x[0]**2 + x[1]**2
@@ -60,17 +64,21 @@ class Optimizer():
     def _print_updates(self, verbose_flag, index, value):
         """
         Private helper method to print out the iteration count and objective function value on all optimizer methods.
+
         INPUTS
         =======
         - verbose_flag: a boolean specifying whether updates about the optimization process will be printed
                 to the console.
         - index: An int specifying the iteration count.
         - value: a float value, specifying the value of the objective function.
+
         RETURNS
         ========
         - None
+
         EXAMPLES
         =========
+
         # Print the value to stdout when verbose_flag is true
         >>> f = lambda x: x
         >>> op = Optimizer(f, np.array([1]))
@@ -89,15 +97,19 @@ class Optimizer():
     def _tolerance_check(self, tolerance, value):
         """
         Private helper method to check if the a given value is lesser than the tolerance threshold.
+
         INPUTS
         =======
         - tolerance: a float value, specifying the threshold against which we need to .
         - value: a float value, specifying the value of the objective function.
+
         RETURNS
         ========
         - True if the L2 norm of the given value is less than tolerance.
+
         EXAMPLES
         =========
+
         # Print the value to stdout when verbose_flag is true
         >>> f = lambda x: x
         >>> op = Optimizer(f, np.array([1]))
@@ -119,6 +131,7 @@ class Optimizer():
     def gd_optimize(self, num_iterations=100, learning_rate=0.01, tolerance=None, verbose=False):
         """
         method that performs gradient descent optimization of the objective function
+
         INPUTS
         =======
         - num_iterations: an int specifying the maximum number of iterations of gradient descent; Default is 100
@@ -128,14 +141,18 @@ class Optimizer():
                 (no tolerance check is used) 
         - verbose: a boolean specifying whether updates about the optimization process will be printed
                 to the console. Default is False
+
         RETURNS
         ========
         - val: the minimum value of the objective_function that was found (float)
         - cur_variable_values: the values for the inputs to objective_function that gave the
                 minimum objective_value found. (1D array of floats with the same size as the number of
                 inputs to the objective function)
+
+
         EXAMPLES
         =========
+
         # multivariate function with scalars as input
         >>> import numpy as np
         >>> f = lambda x, y: x**2 + y**2
@@ -156,6 +173,7 @@ class Optimizer():
         >>> op = Optimizer(f, np.array([1]))
         >>> op.gd_optimize(num_iterations=1000, learning_rate=0.1)
         (1.5134705823042444e-194, array([1.23023192e-97]))
+
         """
 
         cur_variable_values = self.variable_initialization
@@ -178,6 +196,7 @@ class Optimizer():
         """
         Method that performs momentum gradient descent optimization of the objective function. It does so by factoring a
         momentum term during learning, which is an exponential moving average of current and past gradients.
+
         INPUTS
         =======
         - num_iterations: an int specifying the maximum number of iterations of gradient descent; Default is 100
@@ -189,12 +208,15 @@ class Optimizer():
                        (no tolerance check is used)
         - verbose: a boolean specifying whether updates about the optimization process will be printed
                        to the console. Default is False
+
         RETURNS
         ========
         - objective_value: the minimum value of the objective_function that was found (float)
         - cur_variable_values: the values for the inputs to objective_function that gave the
                        minimum objective_value found. (1D array of floats with the same size as the number of
                        inputs to the objective function)
+
+
         EXAMPLES
         =========
         # Univariate objective function with scalar inputs.
@@ -217,6 +239,7 @@ class Optimizer():
         >>> op = Optimizer(g, np.array([0.5, 0.88]), scalar=False)
         >>> op.momentum_optimize(num_iterations=1000, learning_rate=0.01)
         (12.00002667493136, array([2.98791178e-02, 1.51990528e-23]))
+
         """
         if not 0 <= beta <= 1:
             raise ValueError("The value of beta (sample weight) should be between 0 and 1.")
@@ -240,6 +263,7 @@ class Optimizer():
     def adagrad_optimize(self, num_iterations=10000, learning_rate=0.01, fuzz_factor=0.0000001, tolerance=None, verbose=False):
         """
         Method that performs adaptive gradient descent optimization of the objective function.Adagrad adjusts the learning rate         alpha by dividing it by the square root of the cumulative sum of current and past squared gradients.
+
         INPUTS
         =======
         - num_iterations: an int specifying the maximum number of iterations of gradient descent; Default is 100
@@ -250,12 +274,15 @@ class Optimizer():
                        (no tolerance check is used)
         - verbose: a boolean specifying whether updates about the optimization process will be printed
                        to the console. Default is False
+
         RETURNS
         ========
         - objective_value: the minimum value of the objective_function that was found (float)
         - cur_variable_values: the values for the inputs to objective_function that gave the
                        minimum objective_value found. (1D array of floats with the same size as the number of
                        inputs to the objective function)
+
+
         EXAMPLES
         =========
         # Univariate objective function with scalar inputs.
@@ -302,6 +329,7 @@ class Optimizer():
         Method that performs RMSProp gradient descent optimization of the objective function.
         This is an enhancement to Adagrad and adjusts the learning rate alpha by dividing it by the
         exponential moving averages of gradients.
+
         INPUTS
         =======
         - num_iterations: an int specifying the maximum number of iterations of gradient descent; Default is 100
@@ -314,12 +342,15 @@ class Optimizer():
                        (no tolerance check is used)
         - verbose: a boolean specifying whether updates about the optimization process will be printed
                        to the console. Default is False
+
         RETURNS
         ========
         - objective_value: the minimum value of the objective_function that was found (float)
         - cur_variable_values: the values for the inputs to objective_function that gave the
                        minimum objective_value found. (1D array of floats with the same size as the number of
                        inputs to the objective function)
+
+
         EXAMPLES
         =========
         # Univariate objective function with scalar inputs.
