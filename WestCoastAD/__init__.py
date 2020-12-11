92fb0e3808ecc5cc1e7884e467eece94a6657d0e
Yingchen Liu <yingchenliu@Yingchens-MacBook-Pro.local>
2020-12-10 23:41:46 -0700

    Corrected __init__.

--- WestCoastAD/__init__.py
+++ WestCoastAD/__init__.py
@@ -1,5 +1,5 @@
 from .src.forward_mode_AD import Variable
-from .src.forward_mode_AD_auxiliaries import vector_function_jacobian, vector_function_value, multivariate_dimension_check
+from .src.forward_mode_AD_auxiliaries import vector_function_jacobian, vector_function_value, multivariate_dimension_check, differentiate
 from .src.optimizer import Optimizer
 from .src.initializer import Initializer
 from .src.initializer import Zeros
