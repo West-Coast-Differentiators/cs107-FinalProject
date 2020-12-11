96464769e257ad07f81849327aa38eacc6010e20
anita76 <anita.mahinpei@gmail.com>
2020-12-11 13:59:07 -0800

    bfgs

--- WestCoastAD/src/optimizer.py
+++ WestCoastAD/src/optimizer.py
@@ -465,3 +465,80 @@ class Optimizer():
           if self._tolerance_check(tolerance, (learning_rate * der)):
               break
         return val, cur_variable_values
+
+    def bfgs_optimize(self, num_iterations=100, learning_rate=0.01, tolerance=None, verbose=False):
+        """
+        method that performs gradient descent optimization of the objective function
+
+        INPUTS
+        =======
+        - num_iterations: an int specifying the maximum number of iterations of gradient descent; Default is 100
+        - learning_rate: a float/int specifying the learning rate for gradient descent; Default is 0.01
+        - tolerance: a float specifying the smallest tolerance for the updates to the variables. If the L2 norm
+                of the update step is smaller than this value, gradient descent will terminate; Default is None 
+                (no tolerance check is used) 
+        - verbose: a boolean specifying whether updates about the optimization process will be printed
+                to the console. Default is False
+
+        RETURNS
+        ========
+        - val: the minimum value of the objective_function that was found (float)
+        - cur_variable_values: the values for the inputs to objective_function that gave the
+                minimum objective_value found. (1D array of floats with the same size as the number of
+                inputs to the objective function)
+
+
+        EXAMPLES
+        =========
+
+        # multivariate function with scalars as input
+        >>> import numpy as np
+        >>> f = lambda x, y: x**2 + y**2
+        >>> op = Optimizer(f, np.array([1, -1]))
+        >>> op.gd_optimize(num_iterations=1000, learning_rate=0.1)
+        (3.026941164608489e-194, array([ 1.23023192e-97, -1.23023192e-97]))
+
+        # multivariate function with a vector as input
+        >>> import numpy as np
+        >>> f = lambda x: x[0]**2 + x[1]**2
+        >>> op = Optimizer(f, np.array([1, -1]), scalar=False)
+        >>> op.gd_optimize(num_iterations=1000, learning_rate=0.1)
+        (3.026941164608489e-194, array([ 1.23023192e-97, -1.23023192e-97]))
+
+        # univariate function with scalar as input
+        >>> import numpy as np
+        >>> f = lambda x: x**2
+        >>> op = Optimizer(f, np.array([1]))
+        >>> op.gd_optimize(num_iterations=1000, learning_rate=0.1)
+        (1.5134705823042444e-194, array([1.23023192e-97]))
+
+        """
+
+        num_variables = len(self.variable_initialization)
+        cur_variable_values = self.variable_initialization
+        cur_inv_hessian = np.eye(num_variables)
+        val, der = differentiate(self.objective_function, cur_variable_values, self.scalar)
+        
+        for i in range(num_iterations):
+            
+            delta_var = -learning_rate * cur_inv_hessian@der
+            cur_variable_values = cur_variable_values + delta_var
+            val, der2 = differentiate(self.objective_function, cur_variable_values, self.scalar)
+            identity = np.eye(num_variables)
+            y = (der2 - der).reshape(-1, 1)
+            s = delta_var.reshape(-1, 1)
+            denominator = y.T@s
+            t1 = (identity- s@y.T/denominator)
+            t2 = (identity - y@s.T / denominator)
+            t3 = s@s.T/denominator
+            cur_inv_hessian = t1@cur_inv_hessian@t2 + t3
+            der = der2
+
+            if verbose:
+                print("iteration: {}, objective function value: {}".format(i, val))
+
+            if tolerance!=None and np.linalg.norm(delta_var) < tolerance:
+                print("Variable update tolerance was reached. Terminating Search.")
+                break
+        
+        return val, cur_variable_values
